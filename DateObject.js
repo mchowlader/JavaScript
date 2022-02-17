701fb58cac800ae63d3c89494815dedc1e514262
MithunCh <mithun.howlader222@gmail.com>
2022-02-17 12:12:45 +0600

    practices DateObject

---
+++ DateObject.js
@@ -0,0 +1,16 @@
+var dateObject = new Date();
+
+var year = dateObject.getFullYear();
+document.write("Curreant year : " + year + "<br>");
+
+var month = dateObject.getMonth();
+document.write("Curreant month : " + month + "<br>");
+
+var date = dateObject.getDate()
+document.write("Curreant date : " + date + "<br>");
+
+var day = dateObject.getDay();
+document.write("Curreant day : " + day + "<br>");
+
+var hours = dateObject.getHours();
+document.write("Curreant hours : " + hours + "<br>");
--- GuessingGame.js
+++ GuessingGame.js
@@ -5,29 +5,27 @@
 //Run the game for the 5 times
 //Show the number of wons and lost
 
-var count = 0;
-var won = 0;
-var fail = 0;
+var numberOfwon = 0;
+var numberOfFail = 0;
 
 for(var x = 1; x <= 5; x++)
 {
     var guessingNumber = parseInt(prompt("Guessing a number from 1 to 5 : "));
     var randomNumber = Math.floor(Math.random() * 5 + 1);
 
-    count++;
     if(guessingNumber == randomNumber)
     {
         console.log("Yes! Guess the number.")
-        won++;
+        numberOfwon++;
     }
     else
     {
         console.log("You have the lost, guessing numver was ", randomNumber);
-        fail++;
+        numberOfFail++;
         continue;
     }
 }
 
 document.write("You try the game for the 5 times" + "<br>");
-document.write("You won " + won +" times " + "<br>");
-document.write("you failed " + fail +" times"+ "<br>");
+document.write("You won " + numberOfwon +" times " + "<br>");
+document.write("you failed " + numberOfFail +" times"+ "<br>");
